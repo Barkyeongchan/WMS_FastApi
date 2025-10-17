d39ba7a7a08b116ae95986932b5ce4324a202633
Park <xodudcksxo@gmail.com>
2025-10-17 08:23:40 +0000

    드라이버 이름 인식 오류 - 이름 변경으로 해결

--- app/core/database.py
+++ app/core/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
 DB_URL = (
-    f"mysql+mysqlclient://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"mysql+mysqldb://{settings.DB_USER}:{settings.DB_PASSWORD}"
     f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
 
