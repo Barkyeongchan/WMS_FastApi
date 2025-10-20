348c514e7815388d6552d70a06109a56f1b0fd68
Park <xodudcksxo@gmail.com>
2025-10-20 06:41:32 +0000

    main.py 중복 삭제

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.core.config import settings
-
-app = FastAPI(title="WMS FastAPI Server", debug=settings.DEBUG)
-
-@app.get("/")
-def root():
-    return {"message": "FastAPI 서버 실행"}
