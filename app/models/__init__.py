4024375d8eecd32df4e39a2133ba791192471e05
Park <xodudcksxo@gmail.com>
2025-10-20 05:24:08 +0000

    폴더를 패키지로 인식 시킴

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.log_model import Log
+from app.models.category_model import Category
+from app.models.pin_model import Pin
+from app.models.robot_model import Robot
+from app.models.stock_model import Stock
+
+__all__ = ["Log", "Category", "Pin", "Robot", "Stock"]
